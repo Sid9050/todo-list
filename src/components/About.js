9934df07e1b1d5b6843c89ccd278e1ac56cea8eb
Sid9050 <sidchawla2015@gmail.com>
2023-05-23 15:20:16 +0530

    Ready

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div>
+            <ul>
+                <li>You can add your </li>
+            </ul>
+        </div>
+    )
+}
+
+export default About
