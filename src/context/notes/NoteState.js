af98069dc1394af49ae7db4d286b5b62cb56f09a
Sid9050 <sidchawla2015@gmail.com>
2023-05-23 15:41:22 +0530

    Ready

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000"
+  const host = "https://todo-backend-gao2.onrender.com"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
