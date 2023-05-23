ca46bfe62c94689bf21055dfb61a2c8240c5a83d
Sid9050 <sidchawla2015@gmail.com>
2023-05-23 16:11:03 +0530

    Ready

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,7 +11,7 @@ const Signup = (props) => {
         e.preventDefault()
         const { name, email, password, cpassword } = credentials
         if (password === cpassword) {
-            const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            const response = await fetch("https://todo-backend-gao2.onrender.com/api/auth/createuser", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
